6594eab4a7eac09ee81130cc7914f7db0929a983
Centhia Bowman <Centhiabowman@outlook.com>
2020-11-21 15:37:26 -0400

    Agregar archivos de proyecto.

---
+++ TAREA CENTHIA Y YEIDRY/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAREA_CENTHIA_Y_YEIDRY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba el decimal que desee convertir:");
+            int midecimal = int.Parse(Console.ReadLine());
+            string mibinario = "";
+
+            while (midecimal > 0)
+            {
+                mibinario = midecimal % 2 + mibinario;
+
+                midecimal = midecimal / 2;
+
+            }
+
+            Console.WriteLine("El resultado de la conversion es:" + mibinario);
+            Console.ReadLine();
+
+        }
+    }
+}
